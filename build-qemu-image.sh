cd690cf1ce4663d9a52e141f2f235fbaccb0bc99
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2019-09-23 11:58:50 +0200

    Improve qemu image build script

--- build-qemu-image.sh
+++ build-qemu-image.sh
@@ -7,11 +7,14 @@ set -eo pipefail
 "$THIS_DIR"/build-custom-iso.sh -e "INIT_DISK=vda" "$THIS_DIR/ubuntu-18.04-server-amd64/config.sh"
 
 RAM_SIZE=2048
-IMAGE_SIZE=10G
+IMAGE_SIZE=40G
 IMAGE_FORMAT=qcow2
 IMAGE_FILE=$THIS_DIR/ubuntu-18.04-amd64-$RAM_SIZE-$IMAGE_SIZE.$IMAGE_FORMAT
 
+echo "Create QEMU image $IMAGE_FILE"
 qemu-img create "$IMAGE_FILE" -f "$IMAGE_FORMAT" "$IMAGE_SIZE"
+echo "Start QEMU image $IMAGE_FILE"
+echo "Connect to it via VNC: vncviewer localhost:59"
 qemu-system-x86_64 \
     -m "$RAM_SIZE" \
     -name ubuntu-18.04-amd64 \
@@ -23,3 +26,4 @@ qemu-system-x86_64 \
     -smp cpus=2,maxcpus=16,cores=4 \
     -vnc 127.0.0.1:59 \
     -boot once=d
+echo "Done"
